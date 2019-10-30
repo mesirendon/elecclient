c04dcdbce306db380d2b0c49fb39f8256c5c9a5a
Luis De La Peña <lemartinp@unal.edu.co>
2019-10-29 19:50:14 -0500

    Added file loader to documents
            Reported By: Mesias Rendon <amrendonsa@unal.edu.co>

--- src/renderer/components/tender/form/Documents.vue
+++ src/renderer/components/tender/form/Documents.vue
@@ -1,11 +1,29 @@
 <template>
-  <h1>documents</h1>
+  <div>
+    <h1>Documentos</h1>
+    <div class="row">
+      <div class="col-2 offset-10">
+        <button class="btn btn-secondary" @click="load = true">Subir archivo</button>
+      </div>
+    </div>
+    <file-loader class="descriptor" v-if="load"></file-loader>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'Documents',
-  };
+import FileLoader from '@/components/common/FileLoader';
+
+export default {
+  name: 'Documents',
+  data() {
+    return {
+      load: false,
+    };
+  },
+  components: {
+    FileLoader,
+  },
+};
 </script>
 
 <style scoped>
